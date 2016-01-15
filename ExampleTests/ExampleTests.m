310ae43eb0371a1c2e0099491b99a79b0183da9b
Heiko Dreyer <heiko@boxedfolder.com>
2016-01-15 09:58:47 +0100

    Updated header comments

--- ExampleTests/ExampleTests.m
+++ ExampleTests/ExampleTests.m
@@ -3,7 +3,7 @@
 //  ExampleTests
 //
 //  Created by Heiko Dreyer on 26.05.15.
-//  Copyright (c) 2015 boxedfolder.com. All rights reserved.
+//  Copyright (c) 2016 boxedfolder.com. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
